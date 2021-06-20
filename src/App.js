30d0a452baab4746ee0f61e2a9f05f0997a3f843
Jjin SoL <parkleydondon@yahoo.com>
2021-06-19 23:48:25 -0700

    Added 'Add Task' feature

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import { useState } from "react"
 import Header from "./components/Header"
 import Tasks from "./components/Tasks"
+import AddTask from "./components/AddTask"
 
 function App() {
+  const [showAdd, setShowAdd] = useState(false)
+
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -24,6 +27,13 @@ function App() {
     }
   ])
 
+  const addTask = task => {
+    console.log(task)
+    const id = Math.floor(Math.random * 10000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
+
   const deleteTask = id => {
     console.log("delete in app", id)
     setTasks(tasks.filter(task => task.id != id))
@@ -36,7 +46,8 @@ function App() {
 
   return (
     <div className="container">
-      <Header title="Task Tracker" />
+      <Header title="Task Tracker" onAdd={() => setShowAdd(!showAdd)} showAdd={showAdd} />
+      {showAdd && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : "No task to show"}
     </div>
   )
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,15 +2,11 @@ import React from "react"
 import PropTypes from "prop-types"
 import Button from "./Button"
 
-const Header = ({ title }) => {
-  const onClick = () => {
-    console.log("in parent event onClick")
-  }
-
+const Header = ({ title, onAdd, showAdd }) => {
   return (
     <header className="header">
       <h3 className="header">{title}</h3>
-      <Button text="Add" onClick={onClick} />
+      <Button color="green" text={showAdd ? "Close" : "Add"} onClick={onAdd} />
     </header>
   )
 }
